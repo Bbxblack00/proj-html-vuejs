d5dfd66be02acac44face2936e1b839d7d8da443
Benito <bbxblack@gmail.com>
2021-04-29 16:28:59 +0200

    v0.8

--- src/app.js
+++ src/app.js
@@ -3,34 +3,8 @@ function initVue() {
   new Vue({
   el: '#app',
   data: {
-    links: [{
-      nome:'Home',
-      code: '<div class="menu-container">
-
-        <ul>
-          <li>
-            <a href="#">Esempio menu</a>
-          </li>
-
-          <li>
-            <a href="#">Esempio menu</a>
-          </li>
-
-          <li>
-            <a href="#">Esempio menu</a>
-          </li>
-        </ul>'
-    },
-    'Pages', 'Courses', 'features', 'Blog', 'Shop'],
-
-      }
-    ]
+    links: ['Home', 'Pages', 'Courses', 'features', 'Blog', 'Shop'],
     hover: false
-  },
-  methods: {
-    menuDrop: function() {
-
-    }
   }
 });
 }
